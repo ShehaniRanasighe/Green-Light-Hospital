16f2bd849536c40642a891948638a70cf2e5fb53
Shehani Ranasinghe <shehanir1996@gmail.com>
2018-09-13 12:05:29 +0530

    update

--- index.php
+++ index.php
@@ -12,69 +12,16 @@ session_start();
  	<h1 style="font-size: 50px;">Green Light Hospital</h1>
     <p class="block-quote"></p>
     <p><?php include('slideshow.php');?></p>
-  <?php 
-    if(isset($_POST['lemail'])){
-      $i = login($_POST['lemail'],$_POST['lpassword'],"users");
-      if($i==1){
-        echo '<script type="text/javascript"> window.location = "add_patient.php" </script>';
-      }
-    }else if(isset($_POST['remail'])){
-      $i = register($_POST['remail'],$_POST['rpassword'],$_POST['rfullname'],"users");
-      if($i==1){
-        echo '<script type="text/javascript"> window.location = "add_patient.php"</script>';
-      }
-    }else{
-      echo "<div class='alert alert-info'>
-              </div>";
-    }
-    unset($_POST);
-  ?>
-<div class="row">
-  <div class="col col-xl-6 col-sm-6">
-      <h2>Login</h2>
-      <form action="index.php" method="POST">
-        <div class="form-group">
-          <label for="usr">Email:</label>
-          <input type="email" class="form-control" name="lemail" required>
-        </div>
-        <div class="form-group">
-          <label for="pwd">Password:</label>
-          <input type="password" class="form-control" name="lpassword" required>
-        </div>
-        <div class="form-group">
-          <input type="submit" class="btn btn-success" value="Login">
-          <input type="reset" class="btn btn-danger">
-        </div>
-          
-      </form>
-  </div>
-          
-  <div class="col col-xl-6 col-sm-6" id="register1">
-    <form method="post" action="index.php">
-	    <h2>Registration</h2>
-	      <div class="form-group">
-	        <label for="usr">Full Name:</label>
-	        <input type="text" class="form-control" name="rfullname" required>
-	      </div>
-        
-        <div class="form-group">
-	        <label for="usr">Email:</label>
-	        <input type="email" class="form-control" name="remail" required>
-	      </div>
-	          
-        <div class="form-group">
-	        <label for="pwd">Password:</label>
-	        <input type="password" class="form-control"  name="rpassword" required>
-	      </div>
+</div> <br><br>   
 
-	      <div class="form-group">
-	        <input type="submit" class="btn btn-success">
-	        <input type="reset" class="btn btn-danger"></button>
-	      </div>
-    </form>
-  </div>
+<div>
+<iframe src="Highlights.html" height="400" width="425"></iframe>
+<iframe src="career.html" height="400" width="425"></iframe>
+<iframe src="comiunity welness.html" height="400" width="425"></iframe><br><br>
+<p> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62206.24722021342!2d79.94308255574994!3d6.587904364327314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2371ee59557e5%3A0x8b86ba840e8a7b51!2sKalutara!5e0!3m2!1sen!2slk!4v1536779694094" width="100%" height="600" frameborder="0" style="border:0" height="400" width=90%></iframe></p>
+<iframe src="contact icon.html" height="600" width=100%></iframe>
 </div>
-</div>
+ 
 <?php include("footer.php"); ?>
 
 
